44d3595a9b6891756057359f28f43df7781be329
Ben DiFrancesco <ben@scopelift.co>
2023-09-11 11:25:01 -0400

    Make CreatorToken an ERC721 w/ a creator address

--- src/CreatorToken.sol
+++ src/CreatorToken.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract CreatorToken {
+import {ERC721} from "openzeppelin/token/ERC721/ERC721.sol";
 
+contract CreatorToken is ERC721 {
+
+    address public creator;
+
+    constructor(string memory _name, string memory _symbol, address _creator) ERC721(_name, _symbol) {
+        creator = _creator;
+    }
 }
--- test/CreatorToken.t.sol
+++ test/CreatorToken.t.sol
@@ -7,14 +7,20 @@ import {CreatorToken} from "src/CreatorToken.sol";
 contract CreatorTokenTest is Test {
 
   CreatorToken public creatorToken;
+  address public creator = address(0xc2ea702);
+
+  string CREATOR_TOKEN_NAME = "Test Token";
+  string CREATOR_TOKEN_SYMBOL = "TEST";
 
   function setUp() public {
-    creatorToken = new CreatorToken();
+    creatorToken = new CreatorToken(CREATOR_TOKEN_NAME, CREATOR_TOKEN_SYMBOL, creator);
   }
 }
 
 contract Deployment is CreatorTokenTest {
   function test_TokenIsConfiguredAtDeployment() public {
-
+    assertEq(creatorToken.name(), CREATOR_TOKEN_NAME);
+    assertEq(creatorToken.symbol(), CREATOR_TOKEN_SYMBOL);
+    assertEq(creatorToken.creator(), creator);
   }
 }
