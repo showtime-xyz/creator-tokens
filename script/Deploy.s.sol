151ff59a640c7b6c8627a7fc49e8e5d13cc1e9e1
Gary Ghayrat <61768337+garyghayrat@users.noreply.github.com>
2023-10-05 17:08:34 -0400

    Add deploy script (#40)
    * Add deploy script
    
    * Run scopelint fmt
    
    * Clean up
    
    * Remove mapping
    
    * Update README and clean up
    
    * Update `.env.exmaple`
    
    * Update README and `.env.example`

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,9 +4,24 @@
 pragma solidity 0.8.20;
 
 import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
+import {CreatorTokenFactory} from "src/CreatorTokenFactory.sol";
+import {ITestableShowtimeVerifier} from "test/interfaces/ITestableShowtimeVerifier.sol";
 
 contract Deploy is Script {
-  function run() public {
+  /// @notice Deploy the contract
+  function run(address _verifierAddress) public {
+    ITestableShowtimeVerifier _verifier = ITestableShowtimeVerifier(_verifierAddress);
+    if (address(_verifier).code.length == 0) revert("Verifier address is not a contract");
+    // Setup Domain Separator
+    bytes32 _domainSeparator = _verifier.domainSeparator();
+
+    // Deploy the contract
     vm.broadcast();
+
+    CreatorTokenFactory creatorTokenFactory = new CreatorTokenFactory(_verifier, _domainSeparator);
+    require(creatorTokenFactory.domainSeparator() == _domainSeparator, "Domain separator Mismatch");
+
+    console2.log("Deployed contract address %s", address(creatorTokenFactory));
   }
 }
