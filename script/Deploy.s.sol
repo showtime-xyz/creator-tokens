b044af09836d2e5ffcfd60c50568c15ebba5bdd8
garyghayrat <gary.ghayrat@pm.me>
2023-10-17 14:37:36 -0400

    Add  deployment to deploy script's run()

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -11,7 +11,12 @@ import {CreatorTokenSwapRouter} from "src/CreatorTokenSwapRouter.sol";
 
 contract Deploy is Script {
   /// @notice Deploy the contract
-  function run(address _verifierAddress) public {
+  function run(
+    address _verifierAddress,
+    address _universalRouterAddress,
+    address _wethAddress,
+    address _usdcAddress
+  ) public {
     ITestableShowtimeVerifier _verifier = ITestableShowtimeVerifier(_verifierAddress);
     if (address(_verifier).code.length == 0) revert("Verifier address is not a contract");
     // Setup Domain Separator
@@ -23,18 +28,10 @@ contract Deploy is Script {
     CreatorTokenFactory creatorTokenFactory = new CreatorTokenFactory(_verifier, _domainSeparator);
     require(creatorTokenFactory.domainSeparator() == _domainSeparator, "Domain separator Mismatch");
 
-    console2.log("Deployed contract address %s", address(creatorTokenFactory));
-  }
-
-  function deployCreatorTokenSwapRouter(
-    address _universalRouter,
-    address _wethAddress,
-    address _usdcAddress
-  ) public {
-    vm.broadcast();
-
     CreatorTokenSwapRouter creatorTokenSwapRouter =
-      new CreatorTokenSwapRouter(_universalRouter, _wethAddress, _usdcAddress);
+      new CreatorTokenSwapRouter(_universalRouterAddress, _wethAddress, _usdcAddress);
+
+    console2.log("Deployed contract address %s", address(creatorTokenFactory));
     console2.log("Deployed contract address %s", address(creatorTokenSwapRouter));
   }
 }
