7cca4b09ef5e3490926d7d9fcadc168065e94cda
Maxime Richard <max@tryshowtime.com>
2024-03-07 10:42:28 -0800

    Swap router for DEGEN and deploy script

---
+++ script/DeploySwapRouter.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+// slither-disable-start reentrancy-benign
+
+pragma solidity 0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
+import {CreatorTokenSwapRouterDegen} from "src/CreatorTokenSwapRouterDegen.sol";
+
+contract DeploySwapRouter is Script {
+  /// @notice Deploy the contract
+  function run() public {
+    // Base mainnet addresses
+    address univRouterAddress = 0x198EF79F1F515F02dFE9e3115eD9fC07183f02fC;
+    address wethAddress = 0x4200000000000000000000000000000000000006;
+    address degenAddress = 0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed;
+
+    // Deploy the swap router contract
+    vm.broadcast();
+    CreatorTokenSwapRouter creatorTokenSwapRouter =
+      new CreatorTokenSwapRouter(_universalRouterAddress, _wethAddress, _usdcAddress);
+
+    console2.log("Deployed router contract address %s", address(creatorTokenSwapRouter));
+  }
+}
